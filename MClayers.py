331d3ad37966138aea199a819c9eddf4d7d2c355
Nikelui <nikelui@hotmail.it>
2021-07-28 14:48:37 +0200

    Implemented slab tissue

--- MClayers.py
+++ MClayers.py
@@ -11,12 +11,13 @@ import numpy as np
 
 class Layer:
     """Base geometry class, to subclass"""
-    def __init__(self, n=1.4, mua=0, mus=1, order=0, **kwargs):
+    def __init__(self, n=1.4, mua=0, mus=1, order=0, num=0, **kwargs):
         """Defaults to white Monte Carlo with n = 1.4"""
         self.n = n  # refractive index
         self.mua = mua  # absorption coefficient
         self.mus = mus  # scattering coefficient
-        self.order = order  # priority of the layer (0 = lowest)
+        self.order = order  # priority of the layer (0 = lowest). Should be unique
+        self.number = num  # layer identification number. Should be unique
         self.color = kwargs.get('color', 'white')  # Used for graphical representation
         self.detect = kwargs.get('detect', '')  # Used to define detector layers
     
@@ -24,8 +25,37 @@ class Layer:
         """Determine if a point coord = (x,y,z) is inside the geometry or not"""
         pass
     
-    def find_boundary(self, p1, p2):
-        """Find the boundary point between two points of coordinates p1, p2 (if exists)"""
+    def distance(self, photon):
+        """
+        Calculate the distance between the tissue boundary and the photon
+
+        Parameters
+        ----------
+        photon : Photon object
+            An instance of the Photon class
+
+        Returns
+        -------
+        dist : FLOAT.
+            Distance between the photon and the tissue. It is equal to np.inf if the
+            path do not intersect
+        """
+        pass
+    
+    def intersect(self, photon):
+        """
+        Calculate the intersection between the photon path and the tissue boundary.
+
+        Parameters
+        ----------
+        photon : Photon object
+            An instance of the Photon class
+
+        Returns
+        -------
+        intersect : FLOAT ARRAY.
+            Coordinates of the intersection. Returns None if no intersection is found
+        """
         pass
     
     
@@ -38,37 +68,127 @@ class Infinite(Layer):
     
     def is_inside(self, coord):
         return True  # Always true, unless coord = (x,y,z) is in another layer
+    
+    def distance(self, photon):
+        """
+        Calculate the distance between the tissue boundary and the photon.
 
+        Parameters
+        ----------
+        photon : Photon object
+            An instance of the Photon class
+
+        Returns
+        -------
+        dist : FLOAT.
+            Distance between the photon and the tissue. It is equal to np.inf if the
+            path do not intersect
+        """
+        return np.inf  # there is no boundary
+    
+    def intersect(self, photon):
+        """
+        Calculate the intersection between the photon path and the tissue boundary.
+
+        Parameters
+        ----------
+        photon : Photon object
+            An instance of the Photon class
+
+        Returns
+        -------
+        intersect : FLOAT ARRAY.
+            Coordinates of the intersection. Returns None if no intersection is found
+        """
+        return None  # there is no boundary
+    
     
 class Slab(Layer):
     """Horizontal slab of tissue, laying in the xy plane"""
-    def __init__(self):
+    def __init__(self, top=0, thick=1):
         super().__init__()
         self.type = 'slab'
-        self.thickness = 1  # thickness in mm
-        self.pos = 0  # position of the top of the layer in the z axis
+        self.thickness = thick  # thickness in mm
+        self.top = top  # position of the top of the layer in the z axis
     
     def is_inside(self, coord):
-        """Determine if a point coord = (x,y,z) is inside the geometry or not"""
+        """
+        Determine if a point coord = (x,y,z) is inside the geometry or not
+        
+        Parameters
+        ----------
+        coord : FLOAT ARRAY
+            Coordinates of the point.
+
+        Returns
+        -------
+        bool
+            True if the point is contained in the tissue layer, False otherwise.
+        """
         x,y,z = coord  # unpack coordinates
-        if (self.pos + self.thickness) <= z <= self.pos:  # only need to check z
+        if (self.top + self.thickness) <= z <= self.top:  # only need to check z
             return True
         else:
             return False
     
-    def find_boundary(self, p1, p2):
-        """Find the boundary point between two points of coordinates p1, p2 (if exists)"""
-        p1 = np.array(p1)  # to use vector operation
-        p2 = np.array(p2)
-        # First some checks
-        if p1[-1] > p2[-1]:
-            if not(p2[-1] <= self.top <= p1[-1] or p2[-1] <= (self.top + self.thickness) <= p1[-1]):
-                return None
-        elif p2[-1] > p1[-1]:
-            if not(p1[-1] <= self.top <= p2[-1] or p1[-1] <= (self.top + self.thickness) <= p2[-1]):
-                return None
-                
-        v = p2 - p1  # direction vector
+    def distance(self, photon):
+        """
+        Calculate the distance between the tissue boundary and the photon.
+
+        Parameters
+        ----------
+        photon : Photon object
+            An instance of the Photon class
+
+        Returns
+        -------
+        dist : FLOAT.
+            Distance between the photon and the tissue. It is equal to np.inf if the
+            path do not intersect
+        """
+        if photon.direction[2] < 0:  # if directed toward negative z
+            dist = (self.top - photon.coordinates[2]) / photon.direction[2]
+        elif photon.direction[2] > 0:  # if directed toward positive z
+            dist = (self.top + self.thickness - photon.coordinates[2]) / photon.direction[2]
+        else:  # if directed horizontally
+            dist = np.inf
+        return dist
+    
+    def intersect(self, photon):
+        """
+        Calculate the intersection between the photon path and the tissue boundary.
+
+        Parameters
+        ----------
+        photon : Photon object
+            An instance of the Photon class
+
+        Returns
+        -------
+        intersect : FLOAT ARRAY.
+            Coordinates of the intersection. Returns None if no intersection is found
+        """
+        # Sanity check: distance < stepsize
+        dist = self.distance(photon)
+        if dist < photon.step:
+            if photon.direction[2] < 0:  # if directed toward negative z
+                intersect = np.array([
+                    photon.coordinates[0] + dist * photon.direction[0],  # x coordinate
+                    photon.coordinates[1] + dist * photon.direction[1],  # y coordinate
+                    self.top])                                           # z coordinate      
+            elif photon.direction[2] > 0:  # if directed toward positive z
+                intersect = np.array([
+                    photon.coordinates[0] + dist * photon.direction[0],  # x coordinate
+                    photon.coordinates[1] + dist * photon.direction[1],  # y coordinate
+                    self.top + self.thickness])                          # z coordinate
+            else:  # if directed horizontally
+                intersect = None
+        else:
+            intersect = None
+        return intersect
+    
+        
+        
         
         
         
